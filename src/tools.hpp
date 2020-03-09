8a1537d79a79ed3122bbc45afd895ef3c6f9842d
Roc <lrslj@live.com>
2020-03-10 00:55:37 +0800

    Add files via upload

--- src/tools.hpp
+++ src/tools.hpp
@@ -12,11 +12,18 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+namespace vg{
+    void vg_blend(const cv::Mat& image1, const cv::Mat& image2, const cv::Mat & mask1, cv::Mat mask2, OutputArray blendedimage);
 
-void vg_blend(const cv::Mat& image1, const cv::Mat& image2, const cv::Mat & mask1, cv::Mat mask2, OutputArray blendedimage);
+    void vg_drawOn(const cv::Mat& image, cv::Mat& bg, const cv::Mat & mask1);
 
-void vg_drawOn(const cv::Mat& image, cv::Mat& bg, const cv::Mat & mask1);
+    bool isNull(char* str);
 
-bool isNull(char* str);
+    bool isText(char* str);
+
+    cv::Mat erode(cv::Mat mask, int ksize=2);
+}
+
+//dst = cv.erode(binary,kernel)
 
 #endif /* tools_hpp */
